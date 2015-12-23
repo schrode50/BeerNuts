e32a145f4aa649873dfaa3e863069a180f11ac03
Ellyn Howard <ellynhoward@gmail.com>
2015-12-23 10:07:32 -0800

    javascript: fixed the buttons for tivia question to hide certain elements when clicked, added button to play the game again, added questions to the array to populate trivia game.

--- js/trivia.js
+++ js/trivia.js
@@ -12,16 +12,19 @@ var ans3;
 var ansStr3;
 var ans4;
 var ansStr4;
+var playAgain = document.getElementById('playAgain');
 
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('end').className = 'whiteOut';
+  document.getElementById('questionList').style.display='none';
     document.getElementById('start').addEventListener('click', function() {
     var container = document.getElementById('container');//.remove();
-    var start = document.getElementById('start');
-    var intro = document.getElementById('intro');
-    container.removeChild(start);
-    container.removeChild(intro);
+    var start = document.getElementById('start').style.display='none';
+    var intro = document.getElementById('intro').style.display='none';
+    document.getElementById('questionList').style.display='block';
+    // container.removeChild(start);
+    // container.removeChild(intro);
     function addTextNode(text) {
       // var newText = document.createTextNode('Score : ');
       // label = document.getElementById('score-label');
@@ -30,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
     addTextNode();
   })
 });
+
 
   var answer1= document.getElementById('answer1')
   answer1.addEventListener('click',function() {
@@ -40,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
     answer4.textContent='';
     ques1.textContent=' ';
     ques2.textContent='';
+
   })
 
   var answer2 = document.getElementById('answer2')
@@ -138,15 +143,36 @@ document.addEventListener("DOMContentLoaded", function() {
       final.appendChild(scoreNode);
       console.log('hey');
       document.getElementById('end').className ='';
+
+      playAgain.hidden=false;
       // document.getElementById('yay').className = 'gold-stars';
     }, 500);
   };
 
 var questions = [
-  ['Question 1', 'The word beer comes from the Latin word \'bibere\' meaning..?', 'to drink', 'to consume', 'to enjoy', 'to drink', 'to grumble'],
-  ['Question 2', 'Washington State produces what % of the hops used in local production?', '70', '40', '30', '80', '70'],
-  ['Question 3', 'The earliest known chemical evidence of beer dates to circa 3500–3100 BC from which modern day region?', 'Iran', 'Iran', 'Syria', 'Belgium', 'Germany'],
-  ['Question 4', 'What year did brewing begin in the Seattle area?', '1854', '1854', '1874', '1895', '1909'],
-  ['Question 5', 'Pick the one location beer is not available in Japan?', 'drive-throughs', 'vending machines', 'street vendors', 'train stations', 'drive-throughs']
-];
+  ['Question 1', 'The word beer comes from the Latin word \'bibere\' meaning..?', 'to drink', 'to consume', 'to enjoy', 'to drink', 'to grumble','The Latin word bibere means to drink!'],
+  ['Question 2', 'Washington State produces what % of the hops used in local production?', '70', '40', '30', '80', '70', 'Washington state produces 70.8% of the hops used in the U.S.'],
+  ['Question 3', 'The earliest known chemical evidence of beer dates to circa 3500–3100 BC from which modern day region?', 'Iran', 'Iran', 'Syria', 'Belgium', 'Germany', 'Iran has the earliest known chemical evidence of beer!'],
+  ['Question 4', 'What year did brewing begin in the Seattle area?', '1854', '1854', '1874', '1895', '1909', "While the Seattle area is known today as a great beer destination, its brewing history goes back nearly to the city\'s official founding, and far pre-dates Washington\'s statehood. It began in 1854, when Antonio B. Rabbeson founded the Washington Brewery; Seattle was not quite three years old, but the growing logging town needed to start supplying its own beer as its population increased. Within 10 years, the brewery was advertising its 'porter, beer and cream ale.'"],
+  ['Question 5', 'Pick the one location beer is not available in Japan?', 'drive-throughs', 'vending machines', 'street vendors', 'train stations', 'drive-throughs', 'You cannot get beer at drive-throughs in Japan.'],
+  ['Question 6', 'Craft beer in Washington has how much of an economic impact locally?', '1 billion dollars', '1 million dollars', '1 billion dollars', '50 million dollars', '100 million dollars', 'Craft beer has an economic imapact of 1 billion dollars! WOW! That\'s a lot of money!'],
+  ['Question 7', 'The standardized keg is referred to as a 1/2 barrel, how many gallons does this contain?', '15.5', '15.5', '20', '31', '50', 'A 1/2 barrel contains 15.5 U.S. gallons of beer!'],
+  ['Question 8', 'Which country has the most individual beer brands?', 'Belgium', 'Germany', 'England', 'United States', 'Belgium', 'Belgium wins this one, they have 400 beer brands!'],
+  ['Question 9', 'What is the most popular style of craft beer?', 'IPA', 'IPA', 'Porter', 'Blond', 'Amber', 'The IPA is the most popular style!'],
+  ['Question 10', 'Who was the first U.S. President to brew beer in the White House?', 'Barack Obama', 'Barack Obama', 'George Washington', 'Thomas Madison', 'James Madison', 'While George Washington, Thomas Edison and James Madison were all home brewers, Barack Obama is the first president known to ever brew his own beer on the grounds of the White House. He brewed a White House Honey Brown Ale.'],
+  ['Question 11', 'Beer color is directly related to what?', 'Malt', 'Hops', 'Malt', 'Barrel used', 'Time spent fermenting', 'The malted grain is what gives your beer its color! Without getting too technical, the two chemical reactions that most drastically affect beer color are called The Maillard Reaction and Caramelization. This is a chemical reaction that occurs between amino acids and reducing sugars. In beer, The Maillard Reaction happens when you kiln malted barley, which produces chocolate malt and black patent malt. You can expect chocolate, toasted or coffee notes and a darker color from darker malts like these. Caramelization also contributes to a beer’s color. This reaction occurs when you heat the sugar during a boil until it decomposes by itself. Longer boils means darker beer. Along with the darker colors, you can also expect caramelization to cause caramel, buttery or toffee flavors.'],
+  ['Question 12', 'Which state has more breweries in its history than any other?', 'Pennsylvania', 'Pennsylvania', 'Washington', 'Oregon', 'California', 'Pennsylvania has the most prevelant beer history.'],
+  ['Question 13', 'Beer is a rich source for which dietary source?', 'Silicon', 'Silicon', 'Astaxathin', 'Vitamin D', 'Lutein', 'Silicon. Malted barley and hops are rich in Silicon which is a key ingredient for increasing bone mineral density! This can help prevent osteoperosis!'],
+  ['Question 14', ' Which state drinks the most beer?', 'North Dakota', 'North Dakota', 'Montana', 'Washington', 'Utah', 'North Dakota drinks the most beer. North Dakota residents drink 43.3 gallons a year per legal age resident! That is more than double of Utah!'],
+  ['Question 15', 'How many beers can your body break down in one hour?', '1', '1', '2', '3', '4', 'Your body breaks down 1 beer per hour.'],
+  ['Question 16', 'What is the oldest active brewery in the World?', 'Weihenstaphan', 'Heineken', 'Amstel', 'La Fin du Monde', 'Weihenstaphan', 'Weihenstaphan is the oldest active brewery! It was founded in 1040, and is owned by the state of Bavaria today. It is located in Freising, Germany north of Munich.'],
+  ['Question 17', 'The term \'honeymoon\' traces back to where?', 'Babylon', 'Syria', 'Macedonia', 'Babylon', 'Micronesia', 'Brooklyn', 'In ancient Babylon for the month after a wedding the father of the bride would supply his son-in-law with all the mead he could drink. Based on the lunar calender this was called the honey month which then became the honeymoon.'],
+  ['Question 18', "The term 'wet your whistle' was coined in which country?", 'England', 'Germany', 'England', 'Ireleand', 'Scotland', 'The term originated in England centuries ago. At one point they molded clay whistles into the handles of beer mugs which the patrons would blow into to get the bartenders attention for service.'],
+  ['Question 19', 'What is the name of the strongest beer made?', 'Snake Venom', 'Snake Venom', 'Crazy Evil', 'Too Strong for You', 'Medicine', 'As of October 2013 the strongest beer in the world was Snake Venom produced in Scotland with an ABV of 67.5% it is closer in strength to a cask proofed whiskey than a beer.'],
+  ['Question 20', 'Which country has an actual law regarding beer ingredients?', 'Germany', 'Germany', 'France', 'United States', 'Spain', 'Germany as a law on the books called Reinheitsgebot which means literally purity order and originally limited the ingredients to barley, hops, yeast, and water. This was originally put in place to prevent price competition between brewers and bakers and thusly avoid price increases for bread.'],
+  ];
 console.log('questions')
+
+playAgain.addEventListener('click', function(){
+  window.location.reload()
+})
